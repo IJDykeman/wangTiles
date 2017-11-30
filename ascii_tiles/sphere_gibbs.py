ae3e44337e014741e6ab62cd3aa4e405f6d45da1
Isaac Dykeman <ijdykeman@gmail.com>
2017-11-30 09:47:44 -0600

    working on python frontier algorithm

--- ascii_tiles/sphere_gibbs.py
+++ ascii_tiles/sphere_gibbs.py
@@ -26,31 +26,25 @@ def spherehood(i,j):
                 hood[:,1] < SPHERE_WIDTH))
     return hood[condition]
 
-def sphere_probability(i,j, world):
+def sphere_probability(i,j):
     p = np.ones_like(spheres[0,0,0])
     for ni, nj in spherehood(i,j):
-        p += spheres[world[ni, nj], i-ni, j-nj, :] * decided[ni,nj]
-    return p / np.sum(p)
+        p *= spheres[world[ni, nj], i-ni, j-nj, :] * decided[ni,nj] + .0001
+    return (p / (np.sum(p)))
+    # return np.ones_like(p) / len(tiles)
 
 
 def place(i, j, tile_index):
-    # print spheres[tile_index].shape
-    # print max(0, i - SPHERE_WIDTH / 2), min(WORLD_WIDTH, i + SPHERE_WIDTH / 2 + 1)
-    # print max(0, j - SPHERE_WIDTH / 2), min(WORLD_WIDTH, j + SPHERE_WIDTH / 2 + 1)
-    # print probmap[max(0, i - SPHERE_WIDTH / 2): min(WORLD_WIDTH, i + SPHERE_WIDTH / 2 + 1),
-    #               max(0, j - SPHERE_WIDTH / 2): min(WORLD_WIDTH, j + SPHERE_WIDTH / 2 + 1)].shape
-
+    decided[i,j] = 1
+    global probmap
     probmap[max(0, i - SPHERE_WIDTH / 2): min(WORLD_WIDTH, i + SPHERE_WIDTH / 2 + 1),
             max(0, j - SPHERE_WIDTH / 2): min(WORLD_WIDTH, j + SPHERE_WIDTH / 2 + 1)]\
-        *= spheres[tile_index][0]
+        *= spheres[tile_index,
+                   max(0, -(i-SPHERE_WIDTH / 2)) : SPHERE_WIDTH - max(0, i + SPHERE_WIDTH / 2 + 1 - WORLD_WIDTH),
+                   max(0, -(j-SPHERE_WIDTH / 2)) : SPHERE_WIDTH - max(0, j + SPHERE_WIDTH / 2 + 1 - WORLD_WIDTH)][0]
+    # probmap /= np.sum(probmap, axis = -1).reshape(WORLD_WIDTH, WORLD_WIDTH, 1)
     world[i,j] = tile_index
 
-
-def p(i, j, t):
-    p = 1.0
-    for i2, j2 in neighbors(i,j):
-        p *= potential(t, tiles[world[i2, j2]], i2-1, j2-j)
-    return p
 
 def get_all_valid(i,j):
     result = []
@@ -88,7 +82,10 @@ tiles = get_tiles(tile_file_content)
 tile_index_to_prior = np.ones(len(tiles)) / len(tiles)
 
 show_tiles(tiles)
+# quit()
 spheres = create_spheres(tiles)
+
+print "===SPHERES===="
 print spheres.shape
 print spheres[0,:,:,0]
 print spheres[0,:,:,1]
@@ -96,7 +93,6 @@ print spheres[0,:,:,1]
 print "===="
 print spheres[1,:,:,0]
 print spheres[1,:,:,1]
-# print spheres[1]
 
 # show_tiles()
 # print potential(tiles[1], tiles[1], 1,0)
@@ -116,17 +112,48 @@ for i in range(world.shape[0]):
     for j in range(world.shape[1]):
         all_coords.append((i,j))
 
-for step, (i,j) in enumerate(grid_bfs(WORLD_WIDTH / 2, WORLD_WIDTH / 2, WORLD_WIDTH)):
-    if step % 10 == 0:
-        print 
-        print
-        draw_world(world, tiles)
-        time.sleep(.1)
-        # logp(world)
-    decided[i,j] = 1
-    ts, ps = get_tiles_and_probs(i,j,tiles, p)
-    place(i,j, np.random.choice(ts, 1, p=np.array(ps)))
 
-# grid_bfs(starti, startj, width)
+
+place(2,2, 1)
+print world
+draw_world(world, tiles)
+# quit()
+print "=======START GENERATION=========="
+
+step=0
+while np.prod(decided.shape) - np.sum(decided) > 0:
+    step += 1
+    entropy = np.exp(-np.sum((probmap * np.exp(probmap)) * (probmap > 0) * (decided == 0).reshape(WORLD_WIDTH,WORLD_WIDTH,1), axis = -1))
+    # entropy /= np.sum(entropy)
+    entropy += (decided == 1) * np.max(entropy)
+    # print "min", np.min(entropy)
+    print "argmin", np.unravel_index(np.argmin(entropy), entropy.shape)
+    i,j = np.unravel_index(np.argmin(entropy), entropy.shape)
+    if step % 1 == 0:
+        print 
+        print "probs"
+        print probmap[:,:,0]
+        print ""
+        print probmap[:,:,1]
+        print "entropy"
+        print entropy
+        print "world"
+        print world
+        print "decided"
+        print decided
+        draw_world(world, tiles)
+        time.sleep(.2)
+    # print entropy[np.argmin(entropy, axis = (0,1))]
+
+        # print np.prod(decided.shape) - np.sum(decided), "undecided"
+
+    
+    ts, ps = range(len(tiles)), sphere_probability(i,j)
+    print "ps", ps
+    to_place = np.random.choice(ts, 1, p=np.array(ps))
+    print "placing", to_place
+    place(i,j, to_place)
+
+draw_world(world, tiles)
 
 
