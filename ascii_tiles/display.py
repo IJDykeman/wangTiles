4ec6a36fcc8a79a3b75d9c31227be5eb8623b54f
Isaac Dykeman <ijdykeman@gmail.com>
2018-01-06 12:00:18 -0500

    try using autogenerated simplex noise samples as tile set.  Results not promising so far - tile set is very large and not reliably solved.

--- ascii_tiles/display.py
+++ ascii_tiles/display.py
@@ -40,14 +40,16 @@ def draw_world(world, tiles, mask = None):
     # print "=" * WORLD_WIDTH * 3
     if mask is None:
         mask = np.ones_like(world)
-    chars = np.array([[" "] * 3 *world.shape[1]] * 3 * world.shape[0])
+    stride = 2
+    chars = np.array([[" "] * stride * (world.shape[1] + 2)] * stride * (world.shape[0] + 2))
+
     for i in range(world.shape[0]):
         for j in range(world.shape[1]):
             if mask[i,j] == 1:
                 t = tiles[world[i,j]]
-                chars[i*3: i*3+3, j*3:j*3+3] = t
+                chars[i*stride: i*stride+3, j*stride:j*stride+3] = t
             else:
-                chars[i*3: i*3+3, j*3:j*3+3] = np.array([list("   ")]*3)
+                chars[i*stride: i*stride+3, j*stride:j*stride+3] = np.array([list(" "*stride)]*stride)
     chars = list(map(list, chars))
     toprint = []
     for i in range(len(chars)):
