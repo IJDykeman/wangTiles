c071703d20eefda2bbc00575b2082af5c71066c0
IJDykeman <ijdykeman@gmail.com>
2018-02-28 13:13:19 -0600

    add premade waterfall world

--- tiles_3d/constants.py
+++ tiles_3d/constants.py
@@ -1,16 +1,16 @@
 TILE_CONTENT_WIDTH = 12
 TILE_CONTENT_HEIGHT = 4
 
-TILE_WIDTH = 11
+TILE_WIDTH = 7
 SPHERE_WIDTH = 7
-WORLD_WIDTH = 10
+WORLD_WIDTH = 30
 
-SURROUND_BY_AIR = True
+SURROUND_BY_AIR = False
 
 TILESET_FILE = "tiles2.txt"
 
 # VOX_PATH = "/Users/Isaac/Desktop/comp460/tiles_biased_towers_7/"
-# VOX_PATH = "/Users/Isaac/Desktop/comp460/tiles_waterfalls_7/"
-VOX_PATH = "/Users/Isaac/Desktop/comp460/tiles_spaceship_11/"
+VOX_PATH = "/Users/Isaac/Desktop/comp460/tiles_waterfalls_7/"
+# VOX_PATH = "/Users/Isaac/Desktop/comp460/tiles_spaceship_11/"
 
 
